bfb216a8fc248820e187a196df859c3792766444
viztopia <37890050+viztopia@users.noreply.github.com>
2021-08-05 15:48:53 -0400

    update

---
+++ 1-p5-pose-classes-training/sketch.js
@@ -0,0 +1,247 @@
+// Copyright (c) 2019 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* ===
+ml5 Example
+KNN Classification on Webcam Images with poseNet. Built with p5.js
+=== */
+let video;
+// Create a KNN classifier
+const knnClassifier = ml5.KNNClassifier();
+let poseNet;
+let poses = [];
+
+function setup() {
+  const canvas = createCanvas(640, 480);
+  canvas.parent('videoContainer');
+  video = createCapture(VIDEO);
+  video.size(width, height);
+
+  // Create the UI buttons
+  createButtons();
+
+  // Create a new poseNet method with a single detection
+  poseNet = ml5.poseNet(video, modelReady);
+  // This sets up an event that fills the global variable "poses"
+  // with an array every time new poses are detected
+  poseNet.on('pose', function(results) {
+    poses = results;
+  });
+  // Hide the video element, and just show the canvas
+  video.hide();
+}
+
+function draw() {
+  image(video, 0, 0, width, height);
+
+  // We can call both functions to draw all keypoints and the skeletons
+  drawKeypoints();
+  drawSkeleton();
+}
+
+function modelReady(){
+  select('#status').html('model Loaded')
+}
+
+// Add the current frame from the video to the classifier
+function addExample(label) {
+  // Convert poses results to a 2d array [[score0, x0, y0],...,[score16, x16, y16]]
+  const poseArray = poses[0].pose.keypoints.map(p => [p.score, p.position.x, p.position.y]);
+
+  // Add an example with a label to the classifier
+  knnClassifier.addExample(poseArray, label);
+  updateCounts();
+}
+
+// Predict the current frame.
+function classify() {
+  // Get the total number of labels from knnClassifier
+  const numLabels = knnClassifier.getNumLabels();
+  if (numLabels <= 0) {
+    console.error('There is no examples in any label');
+    return;
+  }
+  // Convert poses results to a 2d array [[score0, x0, y0],...,[score16, x16, y16]]
+  const poseArray = poses[0].pose.keypoints.map(p => [p.score, p.position.x, p.position.y]);
+
+  // Use knnClassifier to classify which label do these features belong to
+  // You can pass in a callback function `gotResults` to knnClassifier.classify function
+  knnClassifier.classify(poseArray, gotResults);
+}
+
+// A util function to create UI buttons
+function createButtons() {
+  buttonA = select('#addClassA');
+  buttonA.mousePressed(function() {
+    addExample('1');
+  });
+
+  buttonB = select('#addClassB');
+  buttonB.mousePressed(function() {
+    addExample('2');
+  });
+  
+  buttonC = select('#addClassC');
+  buttonC.mousePressed(function() {
+    addExample('3');
+  });
+  
+  buttonD = select('#addClassD');
+  buttonD.mousePressed(function() {
+    addExample('4');
+  });
+  
+  buttonE = select('#addClassE');
+  buttonE.mousePressed(function() {
+    addExample('5');
+  });
+  
+  buttonF = select('#addClassF');
+  buttonF.mousePressed(function() {
+    addExample('6');
+  });
+
+  // Reset buttons
+  resetBtnA = select('#resetA');
+  resetBtnA.mousePressed(function() {
+    clearLabel('1');
+  });
+	
+  resetBtnB = select('#resetB');
+  resetBtnB.mousePressed(function() {
+    clearLabel('2');
+  });
+  
+  resetBtnC = select('#resetC');
+  resetBtnC.mousePressed(function() {
+    clearLabel('3');
+  });
+	
+  resetBtnD = select('#resetD');
+  resetBtnD.mousePressed(function() {
+    clearLabel('4');
+  });
+  
+  resetBtnE = select('#resetE');
+  resetBtnE.mousePressed(function() {
+    clearLabel('5');
+  });
+	
+  resetBtnF = select('#resetF');
+  resetBtnF.mousePressed(function() {
+    clearLabel('6');
+  });
+  
+  // Predict save
+  buttonPredict = select('#buttonSave');
+  buttonPredict.mousePressed(saveLabels);
+  
+  // Predict load
+  buttonPredict = select('#buttonLoad');
+  buttonPredict.mousePressed(loadLabels);
+
+  // Predict button
+  buttonPredict = select('#buttonPredict');
+  buttonPredict.mousePressed(classify);
+
+  // Clear all classes button
+  buttonClearAll = select('#clearAll');
+  buttonClearAll.mousePressed(clearAllLabels);
+}
+
+// Show the results
+function gotResults(err, result) {
+  // Display any error
+  if (err) {
+    console.error(err);
+  }
+
+  if (result.confidencesByLabel) {
+    console.log(result)
+;    const confidences = result.confidencesByLabel;
+    // result.label is the label that has the highest confidence
+    if (result.label) {
+      select('#result').html(result.label);
+      select('#confidence').html(`${confidences[result.label] * 100} %`);
+    }
+
+    select('#confidenceA').html(`${confidences['1'] ? confidences['L1'] * 100 : 0} %`);
+    select('#confidenceB').html(`${confidences['2'] ? confidences['R1'] * 100 : 0} %`);
+    select('#confidenceC').html(`${confidences['3'] ? confidences['L2'] * 100 : 0} %`);
+    select('#confidenceD').html(`${confidences['4'] ? confidences['R2'] * 100 : 0} %`);
+    select('#confidenceE').html(`${confidences['5'] ? confidences['L3'] * 100 : 0} %`);
+    select('#confidenceF').html(`${confidences['6'] ? confidences['R3'] * 100 : 0} %`);
+  }
+
+  classify();
+}
+
+// Update the example count for each label	
+function updateCounts() {
+  const counts = knnClassifier.getCountByLabel();
+
+  select('#exampleA').html(counts['1'] || 0);
+  select('#exampleB').html(counts['2'] || 0);
+  select('#exampleC').html(counts['3'] || 0);
+  select('#exampleD').html(counts['4'] || 0);
+  select('#exampleE').html(counts['5'] || 0);
+  select('#exampleF').html(counts['6'] || 0);
+}
+
+// Save & Load label JSON
+function saveLabels(){
+  knnClassifier.save("classes.json");
+}
+
+function loadLabels(){
+  knnClassifier.load("classes.json");
+}
+
+
+// Clear the examples in one label
+function clearLabel(classLabel) {
+  knnClassifier.clearLabel(classLabel);
+  updateCounts();
+}
+
+// Clear all the examples in all labels
+function clearAllLabels() {
+  knnClassifier.clearAllLabels();
+  updateCounts();
+}
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints()  {
+  // Loop through all the poses detected
+  for (let i = 0; i < poses.length; i++) {
+    // For each pose detected, loop through all the keypoints
+    let pose = poses[i].pose;
+    for (let j = 0; j < pose.keypoints.length; j++) {
+      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+      let keypoint = pose.keypoints[j];
+      // Only draw an ellipse is the pose probability is bigger than 0.2
+      if (keypoint.score > 0.2) {
+        fill(255, 0, 0);
+        noStroke();
+        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+      }
+    }
+  }
+}
+
+// A function to draw the skeletons
+function drawSkeleton() {
+  // Loop through all the skeletons detected
+  for (let i = 0; i < poses.length; i++) {
+    let skeleton = poses[i].skeleton;
+    // For every skeleton, loop through all body connections
+    for (let j = 0; j < skeleton.length; j++) {
+      let partA = skeleton[j][0];
+      let partB = skeleton[j][1];
+      stroke(255, 0, 0);
+      line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+    }
+  }
+}
---
+++ 2-p5-plateau-ClassifyPose/sketch.js
@@ -0,0 +1,242 @@
+let cnv;
+let waiting = 180;
+
+let classResult = 0;
+let classCache = [];
+let cacheLength = 120;
+
+let maxClass, maxCount;
+let lastClass = 0;
+
+let startedRecording = false;
+let startTime = 0;
+let classTimestamps = [];
+
+let classCacheLengthSlider;
+let recordBtn;
+let downloadBtn;
+let clearBtn;
+
+//------------------ml5 posenet & KNN----------------------
+let video;
+let poseNet;
+let poses = [];
+const knnClassifier = ml5.KNNClassifier();
+let isClassifying = false;
+let loadKNNBtn, classifyBtn;
+let classIndexOffset = 1;
+
+//------------------socket----------
+let socket;
+
+function setup() {
+  cnv = createCanvas(640, 480);
+  cnv.parent('cnvDiv');
+  classCacheLengthSlider = createSlider(10, 180, cacheLength, 10);
+  classCacheLengthSlider.parent('controlsDiv');
+  classCacheLengthSlider.input(() => {
+    cacheLength = classCacheLengthSlider.value()
+    select('#cacheLengthLabel').html(cacheLength);
+  })
+
+  recordBtn = createButton('Start Recording');
+  recordBtn.mousePressed(startStopRecording);
+  recordBtn.parent('controlsDiv');
+
+  clearBtn = createButton('Clear Recording');
+  clearBtn.mousePressed(() => { classTimestamps = []; });
+  clearBtn.parent('controlsDiv');
+
+  downloadBtn = createButton('Download Plateau JSON');
+  downloadBtn.mousePressed(() => { saveJSON(classTimestamps, 'plateau-' + month() + '-' + day() + '-' + hour() + '-' + minute() + '-' + '.json') });
+  downloadBtn.parent('controlsDiv');
+
+
+  //------------PoseNet & KNN----------------------
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  poseNet = ml5.poseNet(video, function () {
+    select('#poseNetStatus').html('PoseNet Loaded')
+  });
+  poseNet.on('pose', function (results) {
+    poses = results;
+  });
+  video.hide();
+
+  loadKNNBtn = select('#buttonLoad');
+  loadKNNBtn.mousePressed(loadLabels);
+
+  classifyBtn = select('#buttonClassify');
+  classifyBtn.mousePressed(toggleClassification);
+
+  //----------setup socket communication---------------------
+  setupSocket();
+}
+
+function startStopRecording() {
+  if (!startedRecording) {
+    startedRecording = true;
+    recordBtn.html('Stop Recording');
+    startTime = millis();
+    classTimestamps.push({ class: maxClass, time: millis() - startTime });
+  } else {
+    startedRecording = false;
+    recordBtn.html('Start Recording');
+    startTime = 0;
+    console.log(classTimestamps);
+  }
+}
+
+function draw() {
+  // background(200);
+  image(video, 0, 0, width, height);
+  drawKeypoints();
+
+  if (frameCount < waiting) {
+    text("Classification will begin in " + waiting + " frames", width / 2 - 100, height / 2);
+  } else {
+    // if (mouseX <= width / 4) {
+    //   classCache.push(1);
+    // } else if (mouseX <= width / 2) {
+    //   classCache.push(2);
+    // } else if (mouseX <= (width * 3) / 4) {
+    //   classCache.push(3);
+    // } else {
+    //   classCache.push(4);
+    // }
+
+    // if (classCache.length >= cacheLength) {
+    //   classCache.shift();
+    // }
+
+    [maxClass, maxCount] = getMaxClass(classCache);
+
+    text("current class is: " + maxClass, width / 2 - 50, height / 2 - 50);
+    text("class count is: " + maxCount, width / 2 - 50, height / 2 + 50);
+
+    //whenever there's a change in class, given the current window length, mark a class timestamp.
+    if (maxClass != lastClass) {
+      print(maxClass + " started at frame " + frameCount);
+      lastClass = maxClass;
+      if (startedRecording) {
+        classTimestamps.push({ class: maxClass, time: millis() - startTime });
+      }
+    }
+  }
+}
+
+function getMaxClass(array) {
+  if (array.length == 0) return [null, null];
+  var modeMap = {};
+  var maxEl = array[0],
+    maxCount = 1;
+  for (var i = 0; i < array.length; i++) {
+    var el = array[i];
+    if (modeMap[el] == null) modeMap[el] = 1;
+    else modeMap[el]++;
+    if (modeMap[el] > maxCount) {
+      maxEl = el;
+      maxCount = modeMap[el];
+    }
+  }
+  return [maxEl, maxCount];
+}
+
+//---------------------PoseNet & KNN stuff----------------------
+function toggleClassification() {
+  if (!isClassifying) {
+    classifyBtn.html('Stop classifying');
+    isClassifying = true;
+    classify();
+
+    socket.emit('plateauOn', true);
+  } else {
+    classifyBtn.html('Start classifying');
+    isClassifying = false;
+
+    socket.emit('plateauOn', false);
+  }
+}
+
+function classify() {
+  const numLabels = knnClassifier.getNumLabels();
+  if (numLabels <= 0) {
+    console.error('There is no examples in any label');
+    return;
+  }
+  const poseArray = poses[0].pose.keypoints.map(p => [p.score, p.position.x, p.position.y]);
+
+  knnClassifier.classify(poseArray, gotResults);
+}
+
+// Show the results
+function gotResults(err, result) {
+  // Display any error
+  if (err) {
+    console.error(err);
+  }
+
+  console.log(result)
+  if (result.confidencesByLabel) {
+    // const confidences = result.confidencesByLabel;
+    // result.label is the label that has the highest confidence
+
+    if (result.label) {
+      const confidences = result.confidences;
+      const idx = parseInt(result.label) + classIndexOffset;
+
+      select('#result').html(idx);
+      select('#confidence').html(`${confidences[parseInt(result.label)] * 100} %`);
+
+      classCache.push(idx);
+      if (classCache.length >= cacheLength) {
+        classCache.shift();
+      }
+    }
+  }
+
+  if (isClassifying) {
+    classify();
+  }
+}
+
+function loadLabels() {
+  knnClassifier.load("classes.json", function () {
+    const numLabels = knnClassifier.getNumLabels();
+    select('#KNNStatus').html(numLabels + " labels loaded.");
+  });
+}
+
+// Clear all the examples in all labels
+function clearAllLabels() {
+  knnClassifier.clearAllLabels();
+  updateCounts();
+}
+
+function drawKeypoints() {
+  // Loop through all the poses detected
+  for (let i = 0; i < poses.length; i++) {
+    // For each pose detected, loop through all the keypoints
+    let pose = poses[i].pose;
+    for (let j = 0; j < pose.keypoints.length; j++) {
+      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+      let keypoint = pose.keypoints[j];
+      // Only draw an ellipse is the pose probability is bigger than 0.2
+      if (keypoint.score > 0.2) {
+        fill(255, 0, 0);
+        noStroke();
+        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+      }
+    }
+  }
+}
+
+//---------------------socket stuff
+function setupSocket() {
+	socket = io.connect('http://127.0.0.1:8081', { port: 8081, rememberTransport: false });
+	socket.on('connect', function () {
+
+		//---Setup OBS OSC---
+		socket.emit('plateauOn', false);
+	});
+}
