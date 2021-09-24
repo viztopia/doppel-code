7f2fbb0a83869765400a0a2c1f46146b6cdbf9ff
Mimi Yin <mimiyin@gmail.com>
2021-09-24 14:51:49 -0400

    Storing calibration to in local storage in both training and classify.

--- 2-p5-plateau-ClassifyPose-new/sketch.js
+++ 2-p5-plateau-ClassifyPose-new/sketch.js
@@ -8,8 +8,8 @@
 
 //------------------socket--------------------
 let socket;
-// let ip = "10.23.11.4";
-let ip = "127.0.0.1"; //the IP of the machine that runs bridge.js
+let ip = "10.23.11.4";
+// let ip = "127.0.0.1"; //the IP of the machine that runs bridge.js
 let port = 8081; //the port of the machine that runs bridge.js
 
 //--------simple UI--------------------
@@ -64,9 +64,9 @@ function preload() { //used for video mode
 }
 
 function setup() {
-  // cnv = createCanvas(1920, 1080);
+  cnv = createCanvas(1920, 1080);
   // cnv = createCanvas(1440, 1080);
-  cnv = createCanvas(960, 540);
+  // cnv = createCanvas(960, 540);
   cnv.parent('cnvDiv');
   classCacheLengthSlider = createSlider(10, 180, cacheLength, 10);
   classCacheLengthSlider.parent('controlsDiv');
@@ -77,15 +77,24 @@ function setup() {
   })
 
   playVidBtn = createButton('Play Video'); //In Progress: used only for playing a video instead of capturing real NiNi via camera
-  playVidBtn.mousePressed(() => { if (video) { console.log("playyyy"); video.loop(); } });
+  playVidBtn.mousePressed(() => {
+    if (video) {
+      console.log("playyyy");
+      video.loop();
+    }
+  });
   playVidBtn.parent('controlsDiv');
 
   clearBtn = createButton('Clear Plateaus Data');
-  clearBtn.mousePressed(() => { plateaus = []; });
+  clearBtn.mousePressed(() => {
+    plateaus = [];
+  });
   clearBtn.parent('controlsDiv');
 
   downloadBtn = createButton('Download Plateau JSON');
-  downloadBtn.mousePressed(() => { saveJSON(plateaus, 'plateaus-' + month() + '-' + day() + '-' + hour() + '-' + minute() + '-' + second() + '.json') });
+  downloadBtn.mousePressed(() => {
+    saveJSON(plateaus, 'plateaus-' + month() + '-' + day() + '-' + hour() + '-' + minute() + '-' + second() + '.json')
+  });
   downloadBtn.parent('controlsDiv');
 
 
@@ -98,13 +107,18 @@ function setup() {
       }
     }
   };
-  video = createCapture(constraints, () => { loadMoveNet(); loadKNN(); });
+  video = createCapture(constraints, () => {
+    loadMoveNet();
+    loadKNN();
+  });
   video.size(width, height);
   video.hide();
 
 
   loadKNNBtn = select('#buttonLoad');
-  loadKNNBtn.mousePressed(() => { loadJSON("classes.json", loadClassesJSON); });
+  loadKNNBtn.mousePressed(() => {
+    loadJSON("classes.json", loadClassesJSON);
+  });
 
   classifyBtn = select('#buttonClassify');
   classifyBtn.mousePressed(toggleClassification);
@@ -117,6 +131,9 @@ function setup() {
 
   //----------setup socket communication---------------------
   setupSocket();
+
+  // Set calibration
+  loadCalibration();
 }
 
 //---------draw-----------------
@@ -145,14 +162,14 @@ function draw() {
 
     //---------------for speed-based delay, send over joint dist----------------
     if (joint && joint.score > jointThreshold) {
-      
+
       let jointDist = dist(joint.x, joint.y, jointPrev.x, jointPrev.y);
       jointPrev = joint;
 
       if (jointDist > 0) {
         // select('#jointDist').elt.innerText = jointDist;
         socket.emit('jointDist', jointDist);
-        
+
       }
     }
 
@@ -161,13 +178,15 @@ function draw() {
 
     if (maxClass) {
       select('#resultDisplay').html(maxClass);
-      let resultCon = round(maxCount/cacheLength*100);
-      select('#resultCon').html("confidence: " +resultCon +"%");
+      let resultCon = round(maxCount / cacheLength * 100);
+      // resultCon = nf(resultCon,3,3);
+      select('#resultCon').html("confidence: " + resultCon + "%");
+      // console.log(round(resultCon));
       // text("current class is: " + maxClass, width / 2 - 50, height / 2 - 50);
       // text("class count is: " + maxCount, width / 2 - 50, height / 2 + 50);
-  
+
     }
-    
+
     //whenever there's a new plateau start, given the current window length & baseline, mark its start time and send new class over.
     if (maxClass && maxCount > newClassCountBaseline && !plateauStarted) {
       console.log(maxClass + " started at frame " + frameCount);
@@ -183,8 +202,12 @@ function draw() {
       plateauStarted = false;
       plateauEndTime = Date.now();
 
-      if ((plateauEndTime - plateauStartTime) > plateauMinLength ){
-        let newPlat = { className: maxClass, start: plateauStartTime, end: plateauEndTime };
+      if ((plateauEndTime - plateauStartTime) > plateauMinLength) {
+        let newPlat = {
+          className: maxClass,
+          start: plateauStartTime,
+          end: plateauEndTime
+        };
         socket.emit('plateauNew', newPlat);
         plateaus.push(newPlat);
       }
@@ -207,7 +230,6 @@ function normalizePoints(x, y) {
   nx = nf((x - minX) / bboxW, 1, 2);
   ny = nf((y - minY) / bboxH, 1, 2);
 }
-
 
 function findKeypoints(pose) {
   minX = Math.min.apply(
@@ -240,12 +262,26 @@ function findKeypoints(pose) {
   );
   bboxW = maxX - minX;
   bboxH = maxY - minY;
+
+  // Store data
+  let calObj = { 'width': bboxW, 'height': bboxH };
+  localStorage.setItem('calibration', JSON.stringify(calObj));
 }
 
+function loadCalibration() {
+  let calibration = JSON.parse(localStorage.getItem('calibration'));
+  console.log("C: ", calibration);
+  if(calibration) {
+    bboxW = calibration.width;
+    bboxH = calibration.height;
+  }
+}
 
 //----------moveNet stuff----------------
 async function loadMoveNet() {
-  const detectorConfig = { modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING };
+  const detectorConfig = {
+    modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING
+  };
   moveNet = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet, detectorConfig);
 
   netReady = true;
@@ -254,7 +290,10 @@ async function loadMoveNet() {
 
 async function estimatePose() {
   const poseEstimation = await moveNet.estimatePoses(video.elt);
-  if (poseEstimation.length > 0) {poses = poseEstimation; joint = poses[0].keypoints[jointNumber]};
+  if (poseEstimation.length > 0) {
+    poses = poseEstimation;
+    joint = poses[0].keypoints[jointNumber]
+  };
 }
 
 //---------KNN stuff------------
@@ -275,10 +314,10 @@ async function classify() {
     return;
   }
   // Convert poses results to a 2d array [[score0, x0, y0],...,[score16, x16, y16]]
-  
-  
-  
-  
+
+
+
+
   const poseArray = poses[0].keypoints.map(p => [p.score, nx, ny]);
 
   const example = tf.tensor(poseArray);
@@ -296,19 +335,20 @@ function gotResults(err, result) {
 
   // console.log(result)
   if (result.label) {
-
+    // console.log(result.label);
     // result.label is the label that has the highest confidence
     const confidences = result.confidences;
-    const idx = parseInt(result.label) + classIndexOffset;
+    const label = result.label;
+    const confidence = round(confidences[label]*100);
 
-    select('#result').html(idx);
-    select('#confidence').html(`${confidences[parseInt(result.label)] * 100} %`);
-
-    classCache.push(idx);
+    select('#result').html(label);
+    select('#confidence').html(confidence + '%');
+    //if (confidence > 0.8) {
+    classCache.push(label);
     while (classCache.length >= cacheLength) {
       classCache.shift();
     }
-
+    //}
   }
 
   if (isClassifying) {
@@ -353,12 +393,15 @@ function toggleClassification() {
 //------------load KNN classes---------------
 function loadClassesJSON(data) {
   if (data) {
-    const { dataset, tensors } = data;
+    const {
+      dataset,
+      tensors
+    } = data;
 
 
     let tensorsData = {};
     Object.keys(dataset).forEach((key) => {
-      // const tensor = 
+      // const tensor =
       const values = Object.keys(tensors[key]).map(v => tensors[key][v]);
       tensorsData[key] = tf.tensor(values, dataset[key].shape, dataset[key].dtype);
     })
@@ -369,8 +412,8 @@ function loadClassesJSON(data) {
 
 //------------draw skeleton keypoints---------------
 function drawKeypoints() {
-   // Loop through all the poses detected
-   for (let i = 0; i < poses.length; i++) {
+  // Loop through all the poses detected
+  for (let i = 0; i < poses.length; i++) {
     // For each pose detected, loop through all the keypoints
     pose = poses[i];
     // console.log(pose.keypoints);
@@ -404,21 +447,24 @@ function drawKeypoints() {
 
 //---------------------socket stuff------------------------------
 function setupSocket() {
-  socket = io.connect('http://' + ip + ':' + port, { port: port, rememberTransport: false });
-  socket.on('connect', function () {
+  socket = io.connect('http://' + ip + ':' + port, {
+    port: port,
+    rememberTransport: false
+  });
+  socket.on('connect', function() {
     socket.emit('plateauOn', false);
   });
 
-  socket.on('disconnect', function () {
+  socket.on('disconnect', function() {
     socket.emit('plateauOn', false);
   });
 
   //-------------In Progress: used for video mode-------------
-  socket.on('playVideo', function (msg) {
+  socket.on('playVideo', function(msg) {
 
   });
 
-  socket.on('scrubVideo', function (msg) {
+  socket.on('scrubVideo', function(msg) {
 
   });
 }
