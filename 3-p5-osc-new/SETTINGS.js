60f1fbbb9c943455df08b93d46019a2b0ae263ec
Mimi Yin <mimiyin@gmail.com>
2021-10-26 08:33:49 -0400

    Settings change.

--- 3-p5-osc-new/SETTINGS.js
+++ 3-p5-osc-new/SETTINGS.js
@@ -19,8 +19,8 @@ const OTHER = 5;
 // Mode names
 const MODENAMES = ['PRESET', 'MANUAL', 'SPEED', 'PLATEAU', 'BOOKMARK', 'OTHER'];
 const MODEBGS = [[140, 226, 238], [147, 186, 225], [137, 132, 214], [114, 81, 178], [164, 188, 188], [200, 200, 200]];
-
-const PRESETS = [60, 0, 4, 4.5, 6, 10];
+// Delay presets
+const PRESETS = [50, 0, 4, 4.5, 6, 10];
 
 // Speed mapping
 const MAXJOINTDIST = []; //no longer using presets for max joint dists. (variables inside mode speed now)
