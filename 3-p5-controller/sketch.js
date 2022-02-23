567f0c28fd9ede3f910734829328d24cef2a9e90
Mimi Yin <mimiyin@gmail.com>
2022-02-22 21:44:01 -0500

    Fixed keypressed for presets.

--- 3-p5-controller/sketch.js
+++ 3-p5-controller/sketch.js
@@ -279,10 +279,10 @@ function keyPressed(e) {
       if (mode == MANUAL || mode == SPEED) modes[mode].adjust(-1);
       break;
     case LEFT_ARROW: //arrow left
-      if (mode == PRESET || mode == SPEED) modes[mode].adjust(-2);
+      if (mode == PRESET || mode == SPEED) modes[mode].update(-2);
       break;
     case RIGHT_ARROW: //arrow right
-      if (mode == PRESET || mode == SPEED) modes[mode].adjust(2);
+      if (mode == PRESET || mode == SPEED) modes[mode].update(2);
       break;
     case 81: //-----------Q: save bookmark 1
       modes[BOOKMARK].save(0, Date.now() - startTime);
